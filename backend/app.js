fdc2378ae297d5146c5bcc93f0e0645d771febaa
checkfelix123 <42866735+checkfelix123@users.noreply.github.com>
2020-04-29 18:37:08 +0200

    modifing app.js

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/images', express.static(path.join('backend/images')));
-/*
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -34,12 +34,11 @@ app.use((req, res, next) => {
         'GET, POST, PATCH, PUT, DELETE, OPTIONS'
     );
     next();
-});*/
+});
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
-var distDir = __dirname + '/dist/';
-app.use('/', express.static(distDir));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
