7af4f360c2e4dd91bdf23fb59ffd1b8a1a447c37
checkfelix123 <42866735+checkfelix123@users.noreply.github.com>
2020-04-29 18:28:14 +0200

    adding metadata

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ mongoose
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/images', express.static(path.join('backend/images')));
-
+/*
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
         'GET, POST, PATCH, PUT, DELETE, OPTIONS'
     );
     next();
-});
+});*/
 
 app.use('/api/posts', postsRoutes);
 app.use('/api/user', userRoutes);
