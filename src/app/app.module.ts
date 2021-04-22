82cb5e6cce7270b4e1a399c90a0ff92529364588
checkfelix123 <graf.felix@aon.at>
2021-04-22 11:29:08 +0200

    removing how-to-use.txt and making app progressive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,48 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { LoginComponent } from './authentication/login/login.component';
-import { SignupComponent } from './authentication/signup/signup.component';
-import { MaterialModule } from './public/material.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import {LoginComponent} from './authentication/login/login.component';
+import {SignupComponent} from './authentication/signup/signup.component';
+import {MaterialModule} from './public/material.module';
 
-import { AuthenticationComponent } from './authentication/authentication.component';
-import { ErrorInterceptor } from './error-interceptor';
+import {AuthenticationComponent} from './authentication/authentication.component';
+import {ErrorInterceptor} from './error-interceptor';
 
-import { AuthInterceptor } from './authorization/auth-interceptor';
-import { ErrorComponent } from './components/error/error.component';
-import { ComponentsModule } from './components/components.module';
+import {AuthInterceptor} from './authorization/auth-interceptor';
+import {ErrorComponent} from './components/error/error.component';
+import {ComponentsModule} from './components/components.module';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HeaderComponent,
-        LoginComponent,
-        SignupComponent,
-        AuthenticationComponent,
-        ErrorComponent,
-    ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    AuthenticationComponent,
+    ErrorComponent,
+  ],
     imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        HttpClientModule,
-        RouterModule,
-        AppRoutingModule,
-        MaterialModule,
-        ComponentsModule,
+      BrowserModule,
+      BrowserAnimationsModule,
+      HttpClientModule,
+      RouterModule,
+      AppRoutingModule,
+      MaterialModule,
+      ComponentsModule,
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: environment.production,
+        // Register the ServiceWorker as soon as the app is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000'
+      }),
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
